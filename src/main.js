03b87d3eab8583ba421ee1df30a965f65608ebbb
liliumL <lijb>
2019-08-20 15:08:30 +0800

    改配置

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import TodoList from './TodoList'
 import Element from 'element-ui'
 Vue.config.productionTip = false
 var axios = require('axios')
-axios.defaults.baseURL = 'http://127.0.0.1:8001'
+axios.defaults.baseURL = 'http://http://94.191.87.159:8080/lilium/'
 axios.defaults.withCredentials = false
 Vue.use(Element)
 // 将API方法绑定到全局
